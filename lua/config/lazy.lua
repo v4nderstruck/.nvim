5d365453669f1fc4f6b086b2661b1f85bfe6cac4
v4nderstruck <v4nderstruck@gmail.com>
2024-04-20 20:19:33 +0200

    aerial

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,6 +12,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
+    { import = "lazyvim.plugins.extras.editor.aerial" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
