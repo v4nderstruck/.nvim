e0a1862e9d387cdcf5396f7ecc2a93b2dbbcffab
v4nderstruck <v4nderstruck@gmail.com>
2025-05-02 14:40:39 +0200

    rm copilot

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,7 +16,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     -- import any extras modules here
