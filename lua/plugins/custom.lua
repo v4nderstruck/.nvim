02b82330083ff550496728389a28bc3de2f2c3d2
v4nderstruck <v4nderstruck@gmail.com>
2025-05-21 10:46:49 +0200

    disable snacks scroll

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,5 +1,18 @@
 return {
   {
+    "snacks.nvim",
+    opts = {
+      indent = { enabled = true },
+      input = { enabled = true },
+      notifier = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = false },
+      statuscolumn = { enabled = false }, -- we set this in options.lua
+      toggle = { map = LazyVim.safe_keymap_set },
+      words = { enabled = true },
+    },
+  },
+  {
     "ThePrimeagen/harpoon",
     keys = {
       {
